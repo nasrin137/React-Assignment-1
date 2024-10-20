c2c0dea4d1d39e3c610edd6c145ad16692fae8be
nasrin137 <nasrinakter.hrm@gmail.com>
2024-10-20 17:43:04 +0800

    added final project

--- src/Pages/Incidents/Incidents.jsx
+++ src/Pages/Incidents/Incidents.jsx
@@ -18,8 +18,8 @@ function Incidents() {
                 <h1 className='title'>Incidents</h1>
             </div>
             <div className="flex-col  md:flex md:flex-row md:justify-between md:items-center relative md:gap-3 pl-10 pb-5">
-            <CiSearch className="absolute top-4 left-12 md:absolute md:top-4 md:left-12" /><input  type="text" className="w-[190px] h-[42px] rounded-md  border-[1px] border-secondary bg-[#FAFAFA] py-3 px-[10px] text-ash pl-8 text-sm" placeholder="Search incident"/>
-            <input  type="text" className="w-[170px] h-[42px] rounded-md  border-[1px] border-secondary bg-[#FAFAFA] py-3 px-[15px] text-ash text-sm" placeholder="Sort By: Date modified"/>
+            <CiSearch className="absolute top-4 left-12 md:absolute md:top-4 md:left-12" /><input  type="text" className="w-[190px] h-[42px] rounded-md  border-[1px] border-gray bg-[#FAFAFA] py-3 px-[10px] text-ash pl-8 text-sm" placeholder="Search incident"/>
+            <input  type="text" className="w-[170px] h-[42px] rounded-md  border-[1px] border-gray bg-[#FAFAFA] py-3 px-[15px] text-ash text-sm" placeholder="Sort By: Date modified"/>
             <button onClick={handleClick} className='bg-primary flex justify-center items-center gap-2 text-white text-sm px-2 py-3 rounded-lg w-[150px] hover:bg-white hover:text-primary transition-all duration-300 mt-5 md:mt-1'>
               <img src="/Images/plus.png" alt="" />New Incident</button>
             
